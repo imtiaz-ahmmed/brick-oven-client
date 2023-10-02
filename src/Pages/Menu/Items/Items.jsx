2d98f9ca65630ecb18096bae146419772ad3d357
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2023-10-03 00:51:10 +0600

    menu page dessert  section added

--- src/Pages/Menu/Items/Items.jsx
+++ src/Pages/Menu/Items/Items.jsx
@@ -3,7 +3,7 @@ import MenuItem from "../../Shared/MenuItem/MenuItem";
 const Items = ({ items }) => {
   return (
     <>
-      <div className="grid md:grid-cols-2 gap-10 mb-12 p-4">
+      <div className="grid md:grid-cols-2 gap-10 my-12 p-4 ">
         {items.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,22 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import bgMenu from "../../../assets/menu/banner3.jpg";
+import bgDessert from "../../../assets/menu/dessert-bg.jpeg";
+import bgSoup from "../../../assets/menu/soup-bg.jpg";
+import bgSalad from "../../../assets/menu/salad-bg.jpg";
+import bgPizza from "../../../assets/menu/pizza-bg.jpg";
+import bgDrinks from "../../../assets/menu/drinks-bg.jpg";
 import useMenu from "../../../Hooks/useMenu";
 import Items from "../Items/Items";
 import SectionTitle from "../../../Components/SharedComponents/SectionTitle";
 const Menu = () => {
   const [menu] = useMenu();
   const offers = menu.filter((item) => item.category === "offered");
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soups = menu.filter((item) => item.category === "soup");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const salads = menu.filter((item) => item.category === "salad");
+  const drinks = menu.filter((item) => item.category === "drinks");
   return (
     <div>
       <Helmet>
@@ -22,6 +32,12 @@ const Menu = () => {
         subHeadings={"Don't Miss"}
       ></SectionTitle>
       <Items items={offers}></Items>
+      <Cover
+        heading="Desserts"
+        description="Would you like to taste our Desserts!"
+        img={bgDessert}
+      ></Cover>
+      <Items items={desserts}></Items>
     </div>
   );
 };
