59377cdf2af80b50fb9b1a00e91c5cebc95fe26e
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2023-10-03 01:29:28 +0600

    view full menu button functionality added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,6 +1,7 @@
 import SectionTitle from "../../../Components/SharedComponents/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 import useMenu from "../../../Hooks/useMenu";
+import { Link } from "react-router-dom";
 
 const PopularMenu = () => {
   const [menu] = useMenu();
@@ -16,6 +17,11 @@ const PopularMenu = () => {
         {popular.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
+      </div>
+      <div className="flex justify-center mb-8">
+        <Link to="/menu" className="btn btn-outline border-0 border-b-4">
+          View Full Menu
+        </Link>
       </div>
     </section>
   );
